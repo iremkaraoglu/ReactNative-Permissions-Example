6ff8c644518c64adc1a9cbd557a978cdb196eb1f
iremkaraoglu <iremk97@gmail.com>
2021-07-24 18:47:06 +0300

    add android support and update ui

--- screens/camPermission.tsx
+++ screens/camPermission.tsx
@@ -4,21 +4,20 @@ import {
   View,
   Text,
   StatusBar,
-  TouchableOpacity
+  Platform
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {request, PERMISSIONS} from 'react-native-permissions';
 
-
-
 export default function CameraPermissionScreen() {
     const [permissionResult, setPermissionResult] = React.useState("Not asked for permission")
 
-    request(PERMISSIONS.IOS.CAMERA).then((result) => {
-      // …
-      setPermissionResult(result)
-      console.log(result)
-    });
+      request(Platform.OS === 'ios' ? PERMISSIONS.IOS.CAMERA : PERMISSIONS.ANDROID.CAMERA).then((result) => {
+        // …
+        setPermissionResult(result)
+        console.log(result)
+      });
+    
 
     return(
         <>
@@ -31,41 +30,81 @@ export default function CameraPermissionScreen() {
             </Text>
             <Text
               style={styles.description}>
-              This app needs access to your camera. If you don't comfortable with
+              This app needs access to your camera. If you are not comfortable with
               this permission, you can go to settings and modify it at any time.
             </Text>
-            <TouchableOpacity style={{backgroundColor: '#DDFFBC', borderRadius: 32, justifyContent: "center", marginTop: 48, alignSelf: "center"}} onPress={() => request}>
-              <Text style={{justifyContent: "center", alignSelf: "center", alignContent: "center", fontSize: 18, fontWeight: "600", color: "#1C281A", margin: 16}}>
-                Ask for permission
-              </Text>
-            </TouchableOpacity>
-            <Text style={{justifyContent: "center", alignSelf: "center", alignContent: "center", fontSize: 18, fontWeight: "600", color: "#DDFFBC", margin: 16}}>
-               Permission Result: {permissionResult}
-              </Text>
+            <View style={{flexDirection: "row"}}>
+            <Text style={{justifyContent: "center", alignSelf: "center", alignContent: "center", fontSize: 18, fontWeight: "600", color: "#DE95BA", margin: 16}}>
+               Permission Result: 
+            </Text>
+            <Text style={{justifyContent: "center", alignSelf: "center", alignContent: "center", fontSize: 18, fontWeight: "600", color: "#FFD9E8", margin: 16}}>
+               {permissionResult}
+            </Text>
+            </View>
+          <View style={{marginHorizontal: 16}}>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#FFD9E8", marginVertical: 8}}>
+            UNAVAILABLE:
+            </Text>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#DE95BA"}}>
+            	This feature is not available (on this device / in this context)
+            </Text>
+          </View>
+          <View style={{marginHorizontal: 16}}>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#FFD9E8", marginVertical: 8}}>
+            DENIED:
+            </Text>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#DE95BA"}}>
+            The permission has not been requested / is denied but requestable
+            </Text>
+          </View>
+          <View style={{marginHorizontal: 16}}>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#FFD9E8", marginVertical: 8}}>
+            GRANTED:
+            </Text>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#DE95BA"}}>
+            The permission is granted
+            </Text>
+          </View>
+          <View style={{marginHorizontal: 16}}>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#FFD9E8", marginVertical: 8}}>
+            LIMITED:
+            </Text>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#DE95BA"}}>
+            The permission is granted but with limitations
+            </Text>
+          </View>
+          <View style={{marginHorizontal: 16}}>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#FFD9E8", marginVertical: 8}}>
+            BLOCKED:
+            </Text>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#DE95BA"}}>
+            The permission is denied and not requestable anymore
+            </Text>
+          </View>
           </View>
         </SafeAreaView>
       </>
     );
 }
 const styles = StyleSheet.create({
-    root: {backgroundColor: '#52734D', flex: 1},
+    root: {backgroundColor: '#4A266A', flex: 1},
     container: {
       alignContent: 'center',
       alignSelf: 'center',
       marginHorizontal: 16,
-      marginTop: 156,
+      marginTop: 40,
     },
     title: {
       fontSize: 24,
       fontWeight: '800',
       alignSelf: 'center',
-      color: '#FEFFDE',
+      color: '#FFD9E8',
     },
     description: {
       fontSize: 21,
       fontWeight: '600',
       alignSelf: 'center',
       marginTop: 32,
-      color: '#FEFFDE',
+      color: '#DE95BA',
     }
   });
--- screens/micPermission.tsx
+++ screens/micPermission.tsx
@@ -4,7 +4,8 @@ import {
   View,
   Text,
   StatusBar,
-  TouchableOpacity
+  ScrollView,
+  Platform
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {request, PERMISSIONS} from 'react-native-permissions';
@@ -14,58 +15,99 @@ import {request, PERMISSIONS} from 'react-native-permissions';
 export default function MicrophonePermissionScreen() {
     const [permissionResult, setPermissionResult] = React.useState("Not asked for permission")
 
-    request(PERMISSIONS.IOS.MICROPHONE).then((result) => {
+    request(Platform.OS === 'ios' ? PERMISSIONS.IOS.MICROPHONE : PERMISSIONS.ANDROID.RECORD_AUDIO).then((result) => {
       // …
       setPermissionResult(result)
       console.log(result)
     });
 
     return(
-        <>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView style={styles.root}>
-          <View
-            style={styles.container}>
-            <Text style={styles.title}>
-              Microphone Permission Needed
+      <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.root}>
+        <ScrollView
+          style={styles.container}>
+          <Text style={styles.title}>
+            Microphone Permission Needed
+          </Text>
+          <Text
+            style={styles.description}>
+            This app needs access to your microphone. If you are not comfortable with
+            this permission, you can go to settings and modify it at any time.
+          </Text>
+          <View style={{flexDirection: "row", marginHorizontal:16}}>
+            <Text style={{justifyContent: "center", alignSelf: "center", alignContent: "center", fontSize: 18, fontWeight: "600", color: "#DE95BA" }}>
+               Permission Result: 
             </Text>
-            <Text
-              style={styles.description}>
-              This app needs access to your microphone. If you don't comfortable with
-              this permission, you can go to settings and modify it at any time.
+            <Text style={{justifyContent: "center", alignSelf: "center", alignContent: "center", fontSize: 18, fontWeight: "600", color: "#FFD9E8", marginHorizontal: 16}}>
+               {permissionResult}
             </Text>
-            <TouchableOpacity style={{backgroundColor: '#DDFFBC', borderRadius: 32, justifyContent: "center", marginTop: 48, alignSelf: "center"}} onPress={() => request}>
-              <Text style={{justifyContent: "center", alignSelf: "center", alignContent: "center", fontSize: 18, fontWeight: "600", color: "#1C281A", margin: 16}}>
-                Ask for permission
-              </Text>
-            </TouchableOpacity>
-            <Text style={{justifyContent: "center", alignSelf: "center", alignContent: "center", fontSize: 18, fontWeight: "600", color: "#DDFFBC", margin: 16}}>
-               Permission Result: {permissionResult}
-              </Text>
           </View>
-        </SafeAreaView>
-      </>
-    );
+          <View style={{marginHorizontal: 16}}>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#FFD9E8", marginVertical: 8}}>
+            UNAVAILABLE:
+            </Text>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#DE95BA"}}>
+            	This feature is not available (on this device / in this context)
+            </Text>
+          </View>
+          <View style={{marginHorizontal: 16}}>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#FFD9E8", marginVertical: 8}}>
+            DENIED:
+            </Text>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#DE95BA"}}>
+            The permission has not been requested / is denied but requestable
+            </Text>
+          </View>
+          <View style={{marginHorizontal: 16}}>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#FFD9E8", marginVertical: 8}}>
+            GRANTED:
+            </Text>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#DE95BA"}}>
+            The permission is granted
+            </Text>
+          </View>
+          <View style={{marginHorizontal: 16}}>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#FFD9E8", marginVertical: 8}}>
+            LIMITED:
+            </Text>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#DE95BA"}}>
+            The permission is granted but with limitations
+            </Text>
+          </View>
+          <View style={{marginHorizontal: 16}}>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#FFD9E8", marginVertical: 8}}>
+            BLOCKED:
+            </Text>
+            <Text style={{fontSize: 18, fontWeight: "600", color: "#DE95BA"}}>
+            The permission is denied and not requestable anymore
+            </Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
 }
 const styles = StyleSheet.create({
-    root: {backgroundColor: '#52734D', flex: 1},
-    container: {
-      alignContent: 'center',
-      alignSelf: 'center',
-      marginHorizontal: 16,
-      marginTop: 156,
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: '800',
-      alignSelf: 'center',
-      color: '#FEFFDE',
-    },
-    description: {
-      fontSize: 21,
-      fontWeight: '600',
-      alignSelf: 'center',
-      marginTop: 32,
-      color: '#FEFFDE',
-    }
-  });
+  root: {backgroundColor: '#4A266A', flex: 1},
+  container: {
+    alignContent: 'center',
+    alignSelf: 'center',
+    marginHorizontal: 16,
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '800',
+    alignSelf: 'center',
+    color: '#FFD9E8',
+  },
+  description: {
+    fontSize: 16,
+    fontWeight: '600',
+    alignSelf: 'center',
+    marginTop: 24,
+    marginBottom:16,
+    color: '#FFD9E8',
+  }
+});
